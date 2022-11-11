e53fa6dc541f3b132e8d2d99899258535b6dca9b
SLeirer <105810795+SLeirer@users.noreply.github.com>
2022-11-11 22:47:09 +0100

    add button and creation of list objects

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 let listeVonToDolists = [];
 
-function addElement(){
-    let completelist=document.getElementById("Todolist");
-    completelist.innerHTML +="<tr><td></td></tr>"
+class List {
+    listName = "";
+    listData = [];
+}
+
+function addButton(){
+    let input = document.getElementById("nameOfList").value;
+    
+    // breaks out of funtion to prevent lists with no names
+    if(input == ""){
+        return;
+    }
+
+    // names are assigned and added to list
+    let list = new List();
+    list.Name = input;
+    listeVonToDolists.push(list);
+    
+    // gives out names of list in console for testing purposes
+    for(let i=0; i < listeVonToDolists.length; i++){
+        console.log(i + " " + listeVonToDolists[i].Name);
+    }
 }
