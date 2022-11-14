2f5caa3c0d042c7347475977e0006e699191bbf7
leirersi <Simon.Leirer@googlemail.com>
2022-11-14 10:21:37 +0100

    functionality of adding lists and display

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-let listeVonToDolists = [];
+// events
+document.getElementById("addNewList").addEventListener('click',addButton);
 
+// global variables
+let listOfTodoLists = [];
+
+// objects
 class List {
     listName = "";
     listData = [];
 }
 
+// functions
 function addButton(){
     let input = document.getElementById("nameOfList").value;
     
@@ -16,10 +22,59 @@ function addButton(){
     // names are assigned and added to list
     let list = new List();
     list.Name = input;
-    listeVonToDolists.push(list);
+    listOfTodoLists.push(list);
     
     // gives out names of list in console for testing purposes
-    for(let i=0; i < listeVonToDolists.length; i++){
-        console.log(i + " " + listeVonToDolists[i].Name);
+    for(let i=0; i < listOfTodoLists.length; i++){
+        console.log(i + " " + listOfTodoLists[i].Name);
     }
+    console.log("___________________");
+
+    //creates table elements
+    updateTodolists();
+}
+
+function updateTodolists(){
+    //creates table elements
+    let tableBody = document.getElementById("TodoListen");
+    let dataHtml = "";
+    let counter = 1;
+
+    //sorting by comparing names of objects
+    listOfTodoLists.sort((a, b) => {
+        let fa = a.Name.toLowerCase();
+        let fb = b.Name.toLowerCase();
+
+        if(fa < fb){
+            return -1;
+        }
+        if(fa > fb){
+            return 1;
+        }
+        return 0;
+    });
+    
+    //creates table content string
+    listOfTodoLists.forEach(element => {
+        dataHtml += "<tr><td width=\"30\">" + counter + "</td><td width=\"60\">" + element.Name +
+                    "</td><td><input type=\"radio\" name=\"listItem\" id=\"listItem"
+                    + (counter-1) + "\"></td></tr>";
+        counter++;
+    })
+
+    console.log(dataHtml);
+
+    tableBody.innerHTML = dataHtml;
+
+    counter = 0;
+
+    listOfTodoLists.forEach(element => {
+        let temp = "listItem" + counter;
+        document.getElementById(temp).addEventListener('click',displayList); 
+        counter++;
+    });
+}
+
+function displayList(){
+    
 }
